a031ccd2e977d6d46d26e0b02c9928b0137968db
unbanksytv <livethelife.tv@gmail.com>
2022-12-04 23:12:30 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,12 +85,14 @@ const Home: NextPage<Props> = ({ fallback }) => {
         {description}
         {image}
       </Head>
+      <div style={{width: '100%', height: '100%', position: 'relative'}}>
       <Image
-          src="coverberamarketdemo.jpeg"
-          alt="Picture of the author"
-          width={3000}
-          height={2000}
-         />
+        alt='Cover'
+        src='/coverberamarketdemo.jpg'
+        layout='fill'
+        objectFit='contain'
+      />
+      </div>
       <header className="col-span-full mb-12 mt-[66px] px-4 md:mt-40 lg:px-0">
         <h1 className="reservoir-h1 text-center dark:text-white">{tagline}</h1>
       </header>
